398576ac6d59a786a72ab6dd4944b0bbb1c9d5f6
Praharsh403 <praharsh.403@gmail.com>
2026-02-21 14:06:37 +0530

    Delete math_module Task2.py

--- math_module Task2.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt,log,sin      #imported certain function's of the module(math) not entire module
-num = float(input("Enter a number: "))   #taking input from user
-res1 = sqrt(num)  #squareroot of the input given by user
-res2 = log(num)   # Logarithm value of input given by user
-res3 = sin(num)   # Sine value of input given by user
-print(f"Squareroot: {res1}")
-print(f"Logarithm: {res2}")
-print(f"Sine: {res3}")
